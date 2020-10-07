b35ca8f0763d04fd31364e45d4b3de0cb6d083e7
es5es5 <es5e5s@nate.com>
2020-10-07 21:19:07 +0900

    init

---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="login">
+    LOGIN
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MainLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="mainLayout">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
