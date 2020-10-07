3ea881b40e728c6efa35e58467a37fae3aa57d95
es5es5 <es5e5s@nate.com>
2020-10-07 22:07:03 +0900

    socialLogin

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,35 @@
 <template>
   <div id="login">
-    LOGIN
+    <form action="" @submit.prevent>
+      <button type="button" @click="socialLogin('google')">GOOGLE</button>
+    </form>
   </div>
 </template>
 
 <script>
+import { authService, firebaseInstance } from '@/plugins/fbase'
+
 export default {
-  name: 'Login'
+  name: 'Login',
+  data () {
+    return {
+      id: '',
+      password: ''
+    }
+  },
+  methods: {
+    async socialLogin (social) {
+      let provider = null
+      switch (social) {
+        case 'google':
+          provider = new firebaseInstance.auth.GoogleAuthProvider()
+          break
+        default:
+          break
+      }
+      await authService.signInWithPopup(provider)
+    }
+  }
 }
 </script>
 
