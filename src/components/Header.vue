67f4d5d0291a237219aae76bae875da75498eeb0
es5es5 <es5e5s@nate.com>
2020-10-11 23:12:13 +0900

    gitmodules

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,7 +7,7 @@
             <img src="@/assets/images/apple-home.png" class="apple_logo" alt="home" @click="goHome">
           </div>
         </div>
-        <div class="grid grid-2-3">
+        <div class="grid grid-2-3 empty">
           <h1 class="header-title">{{ mixinRouteMetaTitle }}</h1>
         </div>
         <div class="grid grid-1-6">
