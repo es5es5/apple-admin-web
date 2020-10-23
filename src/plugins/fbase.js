a166581a2c251d7cc04c7d9573f7d2b7e235b97c
es5es5 <es5e5s@nate.com>
2020-10-23 14:52:20 +0900

    ENV

--- src/plugins/fbase.js
+++ src/plugins/fbase.js
@@ -3,15 +3,33 @@ import 'firebase/auth'
 import 'firebase/firestore'
 import 'firebase/storage'
 
-const firebaseConfig = {
-  apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-  databaseURL: process.env.VUE_APP_DATABASE_URL,
-  projectId: process.env.VUE_APP_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.VUE_APP_MESSAGINGSENDER_ID,
-  appId: process.env.VUE_APP_APP_ID,
-  measurementId: process.env.VUE_APP_MEASUREMENT_ID
+var ENV = process.env.NODE_ENV || 'development'
+ENV = 'ops'
+console.log(ENV)
+
+let firebaseConfig = {}
+if (ENV === 'development') {
+  firebaseConfig = {
+    apiKey: process.env.VUE_APP_API_KEY,
+    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_DATABASE_URL,
+    projectId: process.env.VUE_APP_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_MESSAGINGSENDER_ID,
+    appId: process.env.VUE_APP_APP_ID,
+    measurementId: process.env.VUE_APP_MEASUREMENT_ID
+  }
+} else {
+  firebaseConfig = {
+    apiKey: process.env.VUE_APP_OPS_API_KEY,
+    authDomain: process.env.VUE_APP_OPS_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_OPS_DATABASE_URL,
+    projectId: process.env.VUE_APP_OPS_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_OPS_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_OPS_MESSAGINGSENDER_ID,
+    appId: process.env.VUE_APP_OPS_APP_ID,
+    measurementId: process.env.VUE_APP_OPS_MEASUREMENT_ID
+  }
 }
 
 firebase.initializeApp(firebaseConfig)
